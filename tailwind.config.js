94158affb6e9aa84d4f814dc82602ea44f23b533
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-03-14 11:11:26 +0000

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#00ADB5',
+        secondary: '#3A4750',
+        accent: '#303841',
+        light: '#EEEEEE',
+        dark: '#000000',
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
